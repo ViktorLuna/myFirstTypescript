f651854229bb05f5ac46758735f2fcd530bf7392
Jovi Joshua Tania <jovijoshuatania027@gmail.com>
2024-05-23 13:31:38 +0800

    052324-TUPLES

--- src/app.ts
+++ src/app.ts
@@ -19,12 +19,14 @@ const list = new listTemplate(ul)
 form.addEventListener('submit',(e: Event) => {
     e.preventDefault();
 
+    let values:[string, string, number] = [tofrom.value, details.value, amount.valueAsNumber]
+
     let doc: Hasformatter;
 
     if(type.value == 'invoice'){
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }else{
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
 
     // console.log(doc)
